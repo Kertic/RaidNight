fbd3aeaf7d95fdaa0e29f291bea4bcfb2d0df06d
Brennan Rodriguez <yougotbrennan@gmail.com>
2023-11-13 22:51:25 -0600

    boilers for weapon

---
+++ Assets/Code/Player/Weapon/Weapon.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Code.Player.Weapon
+{
+    public class Weapon : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject projectilePrefab;
+    
+        
+    }
+}
